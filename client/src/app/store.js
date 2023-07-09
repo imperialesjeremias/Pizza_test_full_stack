cde3f5dc5a423773088110882ee37de3f22f3713
Imperiales Jeremías Leon <97626561+imperialesjeremias@users.noreply.github.com>
2023-07-09 18:35:27 -0300

    ingredienteSlice, PizzaSlice, act store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../feactures/auth/authSlice"
+import authReducer from "../feactures/auth/authSlice";
+import pizzaReducer from "../feactures/pizzas/pizzasSlice";
+import ingredientesReducer from "../feactures/ingredientes/ingredientesSlice";
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        pizzas: pizzaReducer,
+        ingredientes: ingredientesReducer
     }
 });
---
+++ client/src/feactures/pizzas/pizzasSlice.js
@@ -0,0 +1,61 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {getPizzas, getPizzaDetalle, addIngrediente} from "../../api/pizzas";
+import {createSelector} from "reselect";
+
+export const fetchPizzas= createAsyncThunk(
+    "pizzas/fetchPizzas",
+    async () => {
+        const res = await getPizzas();
+        return res.data.pizzas;
+    }
+);
+
+export const fetchPizzaDetalle = createAsyncThunk(
+    "pizzas/fetchPizzaDetalle",
+    async (id) => {
+        const res =  await getPizzaDetalle(id);
+        return res.data;
+    }
+);
+
+export const createPizza = createAsyncThunk(
+    "pizzas/createPizza",
+    async (pizzaData) => {
+        const res = await createPizza(pizzaData);
+        return res.data;
+    }
+);
+
+export const addIngrediente = createAsyncThunk(
+    "pizzas/addIngrediente",
+    async (pizzaId, ingredienteId) => {
+        const res = await addIngrediente(pizzaId, ingredienteId);
+        return res.data;
+    }
+);
+
+const pizzasSlice = createSlice({
+    name: "pizzas",
+    initialState: {},
+    reducers: {},
+    extraReducers: (builded) => {
+        builded.addCase(fetchPizzas.fulfilled, (state, action) => {
+            return action.payload;
+        });
+        builded.addCase(createPizza.fulfilled, (state, action) => {
+            console.log(actionl.payload);
+        });
+        builded.addCase(fetchPizzaDetalle.fulfilled, (state, action) => {
+            return {...state, pizzaDetalle: action.payload};
+        });
+    },
+    immer: false,
+});
+
+export default pizzasSlice.reducer;
+
+export const selectPizzaDetalle = createSelector(
+    (state) => state.pizzas,
+    (pizzas) => pizzas.pizzaDetalle
+);
+
